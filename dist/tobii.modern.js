b640af744c7215c11915006fc97c2c36b2728fb4
François <f.mably@gmail.com>
2025-02-11 12:27:13 +0100

    build

--- dist/tobii.modern.js
+++ dist/tobii.modern.js
@@ -450,6 +450,7 @@ function Tobii(userOptions) {
       close: true,
       closeText: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false"><path stroke="none" d="M0 0h24v24H0z"/><line x1="18" y1="6" x2="6" y2="18" /><line x1="6" y1="6" x2="18" y2="18" /></svg>',
       closeLabel: 'Close lightbox',
+      dialogTitle: 'Lightbox',
       loadingIndicatorLabel: 'Image loading',
       counter: true,
       download: false,
@@ -704,7 +705,7 @@ function Tobii(userOptions) {
     lightbox.setAttribute('role', 'dialog');
     lightbox.setAttribute('aria-hidden', 'true');
     lightbox.setAttribute('aria-modal', 'true');
-    lightbox.setAttribute('aria-label', 'Lightbox');
+    lightbox.setAttribute('aria-label', userSettings.dialogTitle);
     lightbox.classList.add('tobii');
 
     // Adc theme class
